3c858090dd4745d216e1f394d35262029eeb31e3
GavG <gavingration@gmail.com>
2020-12-30 14:41:48 +0000

    wip

--- turn.sh
+++ turn.sh
@@ -1,1 +1,1 @@
-turnserver -v --syslog --cli-password secred --max-bps=3000000 --fingerprint --relay-threads 3 --min-port=31337 --max-port=32000 --db="var/db/turndb" --user ubuntu:ubuntu --realm coolrealm --lt-cred-mech
+turnserver -v --syslog --cli-password secred --max-bps=3000000 --fingerprint --relay-threads 3 --min-port=31337 --max-port=32000 --db="var/db/turndb" --user ubuntu:slipstream --realm coolrealm --lt-cred-mech
