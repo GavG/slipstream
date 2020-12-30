e75e0afb8039ec22e0dff8906938961664664ab3
GavG <gavingration@gmail.com>
2020-12-30 18:13:27 +0000

    wip

--- 5060.sh
+++ 5060.sh
@@ -1,1 +1,2 @@
-turnserver -v --syslog --cli-password secred --max-bps=3000000 --fingerprint --relay-threads 3 --min-port=31337 --max-port=32000 --no-tcp --no-tls --no-dtls --user ubuntu:slipstream --realm coolrealm --lt-cred-mech -p 5060
+turnserver -v --syslog --cli-password secred --max-bps=3000000 --fingerprint --relay-threads 3 --min-port=31337 --max-port=32000 --no-tcp --no-tls --no-dtls --user ubuntu:slipstream --realm coolrealm --lt-cred-mech -p 5060 --cert=/etc/letsencrypt/live/blog.decloak.live/fullchain.pem --pkey=
+ /etc/letsencrypt/live/blog.decloak.live/privkey.pem
--- turn.sh
+++ turn.sh
@@ -1,1 +1,2 @@
-turnserver -v --syslog --cli-password secred --max-bps=3000000 --fingerprint --relay-threads 3 --min-port=31337 --max-port=32000 --user ubuntu:slipstream --realm coolrealm --lt-cred-mech
+turnserver -v --syslog --cli-password secred --max-bps=3000000 --fingerprint --relay-threads 3 --min-port=31337 --max-port=32000 --user ubuntu:slipstream --realm coolrealm --lt-cred-mech --cert=/etc/letsencrypt/live/blog.decloak.live/fullchain.pem --pkey=
+ /etc/letsencrypt/live/blog.decloak.live/privkey.pem
