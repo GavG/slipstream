1eb0b0a782dbfcdaa8189147770f9c9a89ce73c7
GavG <gavingration@gmail.com>
2020-12-30 18:22:12 +0000

    revert

--- 5060.sh
+++ 5060.sh
@@ -1,2 +1,1 @@
-turnserver -v --syslog --cli-password secred --max-bps=3000000 --fingerprint --relay-threads 3 --min-port=31337 --max-port=32000 --no-tcp --no-tls --no-dtls --user ubuntu:slipstream --realm blog.decloak.live --lt-cred-mech -p 5060 --cert=/etc/letsencrypt/live/blog.decloak.live/fullchain.pem --pkey=
- /etc/letsencrypt/live/blog.decloak.live/privkey.pem
+turnserver -v --syslog --cli-password secred --max-bps=3000000 --fingerprint --relay-threads 3 --min-port=31337 --max-port=32000 --no-tcp --no-tls --no-dtls --user ubuntu:slipstream --realm coolrealm --lt-cred-mech -p 5060
